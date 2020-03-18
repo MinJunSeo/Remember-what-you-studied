b2a99ee3df7e63c0d88e3bd4321b8c8d97732986
MinJun Seo <smj0385@naver.com>
2020-03-18 15:08:53 +0900

    docs : Add resource file
    Add resource file that option dialog design.

---
+++ 2020/SimplePaint/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Resource.rc에서 사용되고 있습니다.
+//
+#define IDD_DIALOG1                     101
+#define IDC_RED                         1001
+#define IDC_YELLOW                      1002
+#define IDC_BLUE                        1003
+#define IDC_BLACK                       1004
+#define IDC_THICK                       1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
