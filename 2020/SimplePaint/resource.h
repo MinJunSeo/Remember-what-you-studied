1e1020fda81e8f6281600eed60bfb79b7bfface2
MinJun Seo <smj0385@naver.com>
2020-03-19 14:13:16 +0900

    docs : Update SimplePaint version to 3.0
    I change resource files to latest version.

--- 2020/SimplePaint/resource.h
+++ 2020/SimplePaint/resource.h
@@ -3,11 +3,12 @@
 // Resource.rc에서 사용되고 있습니다.
 //
 #define IDD_DIALOG1                     101
-#define IDC_RED                         1001
-#define IDC_YELLOW                      1002
-#define IDC_BLUE                        1003
-#define IDC_BLACK                       1004
-#define IDC_THICK                       1005
+#define IDC_THICK                       1001
+#define IDC_RED                         1002
+#define IDC_YELLOW                      1003
+#define IDC_BLUE                        1004
+#define IDC_BLACK                       1005
+#define IDC_CLEAR                       1006
 
 // Next default values for new objects
 // 
@@ -15,7 +16,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
