3901cf98544dcd6cc1531feafdfb49dccd9b4aa9
MinJun Seo <smj0385@naver.com>
2020-03-20 11:41:58 +0900

    docs : Update resource files to version 3.1
    Change resource files to latest version.

--- 2020/SimplePaint/resource.h
+++ 2020/SimplePaint/resource.h
@@ -8,7 +8,12 @@
 #define IDC_YELLOW                      1003
 #define IDC_BLUE                        1004
 #define IDC_BLACK                       1005
-#define IDC_CLEAR                       1006
+#define IDC_USERDEF                     1006
+#define IDC_CLEAR                       1007
+#define IDC_SCRRED                      1009
+#define IDC_SCRGREEN                    1010
+#define IDC_SCRBLUE                     1011
+#define IDC_INFORM                      1013
 
 // Next default values for new objects
 // 
@@ -16,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
