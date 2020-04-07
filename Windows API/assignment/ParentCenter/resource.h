b642f0938256b6fc9c8017244d3218f83d99c44b
MinJun Seo <smj0385@naver.com>
2020-04-07 11:42:22 +0900

    Rename 2020/ParentCenter/resource.h to Windows API/assignment/ParentCenter/resource.h

--- Windows API/assignment/ParentCenter/resource.h
+++ Windows API/assignment/ParentCenter/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++¿¡¼­ »ý¼ºÇÑ Æ÷ÇÔ ÆÄÀÏÀÔ´Ï´Ù.
-// Resource.rc¿¡¼­ »ç¿ëµÇ°í ÀÖ½À´Ï´Ù.
+// Microsoft Visual C++ì—ì„œ ìƒì„±í•œ í¬í•¨ íŒŒì¼ìž…ë‹ˆë‹¤.
+// Resource.rcì—ì„œ ì‚¬ìš©ë˜ê³  ìžˆìŠµë‹ˆë‹¤.
 //
 #define IDD_DIALOG1                     101
 #define IDC_RED                         1001
