9b5b2b5920e0d97e6a813556f87f31fb46b6255d
MinJun Seo <smj0385@naver.com>
2020-05-14 13:15:54 +0900

    docs : Update file to version 1.1
    데이터베이스와 연동하여 데이터베이스 테이블 목록을 웹 페이지에서 보여줄 수 있도록 하였습니다.

--- Node.js/opentutorial_Nodejs/main.js
+++ Node.js/opentutorial_Nodejs/main.js
@@ -5,28 +5,44 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
 
-var app = http.createServer(function(request,response){
+var db = mysql.createConnection({
+  host : "localhost",
+  user : "root",
+  password : "smj6689*",
+  database : "opentutorials"
+});
+db.connect();
+
+var app = http.createServer(function(request,response)
+{
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+    if(pathname === '/')
+    {
+      if(queryData.id === undefined)
+      {
+        db.query(`SELECT * FROM topic`, function(error, topics)
+        {
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
+            `<a href = "/create">create</a>`);
           response.writeHead(200);
           response.end(html);
         });
-      } else {
-        fs.readdir('./data', function(error, filelist){
+      }
+      else
+      {
+        fs.readdir('./data', function(error, filelist)
+        {
           var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description)
+          {
             var title = queryData.id;
             var sanitizedTitle = sanitizeHtml(title);
             var sanitizedDescription = sanitizeHtml(description, {
@@ -47,8 +63,11 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-    } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
+    }
+    else if(pathname === '/create')
+    {
+      fs.readdir('./data', function(error, filelist)
+      {
         var title = 'WEB - create';
         var list = template.list(filelist);
         var html = template.HTML(title, list, `
@@ -65,24 +84,33 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(html);
       });
-    } else if(pathname === '/create_process'){
+    }
+    else if(pathname === '/create_process')
+    {
       var body = '';
-      request.on('data', function(data){
+      request.on('data', function(data)
+      {
           body = body + data;
       });
-      request.on('end', function(){
+      request.on('end', function()
+      {
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err)
+          {
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
-          })
+          });
       });
-    } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
+    }
+    else if(pathname === '/update')
+    {
+      fs.readdir('./data', function(error, filelist)
+      {
         var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description)
+        {
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.HTML(title, list,
@@ -104,38 +132,51 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       });
-    } else if(pathname === '/update_process'){
+    }
+    else if(pathname === '/update_process')
+    {
       var body = '';
-      request.on('data', function(data){
+      request.on('data', function(data)
+      {
           body = body + data;
       });
-      request.on('end', function(){
+      request.on('end', function()
+      {
           var post = qs.parse(body);
           var id = post.id;
           var title = post.title;
           var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          fs.rename(`data/${id}`, `data/${title}`, function(error)
+          {
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err)
+            {
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
-            })
+            });
           });
       });
-    } else if(pathname === '/delete_process'){
+    }
+    else if(pathname === '/delete_process')
+    {
       var body = '';
-      request.on('data', function(data){
+      request.on('data', function(data)
+      {
           body = body + data;
       });
-      request.on('end', function(){
+      request.on('end', function()
+      {
           var post = qs.parse(body);
           var id = post.id;
           var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
+          fs.unlink(`data/${filteredId}`, function(error)
+          {
             response.writeHead(302, {Location: `/`});
             response.end();
-          })
+          });
       });
-    } else {
+    }
+    else
+    {
       response.writeHead(404);
       response.end('Not found');
     }
