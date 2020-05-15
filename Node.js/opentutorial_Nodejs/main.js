03c2346849defdecd3c098dee958dfa522bd3872
MinJun Seo <smj0385@naver.com>
2020-05-15 15:19:22 +0900

    docs : Update README.md
    main.js를 1.5 버전으로 업데이트하였습니다.

--- Node.js/opentutorial_Nodejs/main.js
+++ Node.js/opentutorial_Nodejs/main.js
@@ -169,18 +169,19 @@ var app = http.createServer(function(request,response)
         var body = '';
         request.on('data', function(data)
         {
-                body = body + data;
+            body = body + data;
         });
         request.on('end', function()
         {
-                var post = qs.parse(body);
-                var id = post.id;
-                var filteredId = path.parse(id).base;
-                fs.unlink(`data/${filteredId}`, function(error)
-                {
-                    response.writeHead(302, {Location: `/`});
-                    response.end();
-                });
+            var post = qs.parse(body);
+
+            db.query('DELETE FROM topic WHERE id=?', [post.id], function(error, result)
+            {
+                if(error) { throw error; }
+
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            });
         });
     }
     else
