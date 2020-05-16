dd0b705bac19ec8ae0e33479e5763ddb1227191b
MinJun Seo <smj0385@naver.com>
2020-05-16 17:31:48 +0900

    docs : Update main.js to version 1.6
    main.js를 1.6 버전으로 업데이트하였습니다.

--- Node.js/opentutorial_Nodejs/main.js
+++ Node.js/opentutorial_Nodejs/main.js
@@ -46,7 +46,7 @@ var app = http.createServer(function(request,response)
             {
                 if(error) { throw error; }
 
-                db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic)
+                db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic)
                 {
                     if(error2) { throw error2; }
 
@@ -54,7 +54,9 @@ var app = http.createServer(function(request,response)
                     var description = topic[0].description;
                     var list = template.list(topics);
                     var html = template.HTML(title, list,
-                        `<h2>${title}</h2>${description}`,
+                        `<h2>${title}</h2>
+                        ${description}
+                        <p>by ${topic[0].name}</p>`,
                         `<a href = "/create">create</a>
                         <a href = "/update?id=${queryData.id}">update</a>
                         <form action = "delete_process" method = "POST">
