107ed4cb1fd37926abcf2d0f74abd1f7dfb9a716
MinJun Seo <smj0385@naver.com>
2020-05-17 17:02:31 +0900

    docs : Update template.js
    template.js를 1.8 버전으로 업데이트하였습니다.
    
    웹 사이트에서 현재 보고 있는 파일의 내용을 수정할 때 해당 파일을 작성한 글쓴이로 체크 박스가 자동으로 선택되도록 템플릿을 수정하였습니다.

--- Node.js/opentutorial_Nodejs/lib/template.js
+++ Node.js/opentutorial_Nodejs/lib/template.js
@@ -1,47 +1,54 @@
 module.exports = {
-  HTML : function(title, list, body, control)
-  {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list : function(topics)
-  {
-    var list = '<ul>';
-    var i = 0;
-    while(i < topics.length){
-      list = list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
-      i = i + 1;
-    }
-    list = list+'</ul>';
-    return list;
-  },
-  authorSelect : function(authors)
-  {
-    var tag = '';
-    var i = 0;
-
-    while(i < authors.length)
+    HTML : function(title, list, body, control)
     {
-        tag += `<option value = "${authors[i].id}">${authors[i].name}</option>`;
-        i++;
-    }
+        return `
+        <!doctype html>
+        <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${control}
+            ${body}
+        </body>
+        </html>
+        `;
+    },
+    list : function(topics)
+    {
+        var list = '<ul>';
+        var i = 0;
+        while(i < topics.length)
+        {
+            list = list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
+            i = i + 1;
+        }
+        list = list+'</ul>';
+        return list;
+    },
+    authorSelect : function(authors, author_id)
+    {
+        var tag = '';
+        var i = 0;
 
-    return`
-    <select name = "author">
-        ${tag}
-    </select>`;
-  }
+        while(i < authors.length)
+        {
+            var selected = '';
+
+            if(authors[i].id === author_id)
+            {
+                selected = ' selected';
+            }
+            tag += `<option value = "${authors[i].id}"${selected}>${authors[i].name}</option>`;
+            i++;
+        }
+
+        return`
+        <select name = "author">
+            ${tag}
+        </select>`;
+    }
 }
