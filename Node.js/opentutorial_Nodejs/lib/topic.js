c7cfb0b81319000faaeb99d504202fbac3b2cc3c
MinJun Seo <smj0385@naver.com>
2020-05-25 14:18:22 +0900

    docs : Update topic.js
    추가 기능
    1. 웹 페이지 상에서 글 추가 기능 라이브러리 구현
    2. 웹 페이지 상에서 글 내용 변경 기능 라이브러리 구현
    3. 웹 페이지 상에서 파일 삭제 기능 라이브러리 구현

--- Node.js/opentutorial_Nodejs/lib/topic.js
+++ Node.js/opentutorial_Nodejs/lib/topic.js
@@ -1,7 +1,9 @@
 var db = require('./db.js');
 var template = require('./template.js');
+var url = require('url');
+var qs = require('querystring');
 
-exports.home = function(reqyuest, response)
+exports.home = function(request, response)
 {
     db.query(`SELECT * FROM topic`, function(error, topics)
     {
@@ -16,5 +18,182 @@ exports.home = function(reqyuest, response)
 
         response.writeHead(200);
         response.end(html);
+    });
+}
+
+exports.page = function(request, response)
+{
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+
+    db.query(`SELECT * FROM topic`, function(error, topics)
+    {
+        if(error) { throw error; }
+
+        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(error2, topic)
+        {
+            if(error2) { throw error2; }
+
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(title, list,
+                `<h2>${title}</h2>
+                ${description}
+                <p>by ${topic[0].name}</p>`,
+                `<a href = "/create">create</a>
+                <a href = "/update?id=${queryData.id}">update</a>
+                <form action = "delete_process" method = "POST">
+                    <input type = "hidden" name = "id" value = "${queryData.id}">
+                    <input type = "submit" value = "delete">
+                </form>`);
+            
+            response.writeHead(200);
+            response.end(html);
+        });
+    });
+}
+
+exports.create = function(request, response)
+{
+    db.query(`SELECT * FROM topic`, function(error, topics)
+    {
+        if(error) { throw error; }
+        
+        db.query(`SELECT * FROM author`, function(error2, authors)
+        {
+            if(error2) { throw error2; }
+
+            var title = 'WEB - create';
+            var list = template.list(topics);
+            var html = template.HTML(title, list,
+                `<form action = "/create_process" method = "POST">
+                    <p>
+                        <input type = "text" name = "title" placeholder = "title">
+                    </p>
+                    <p>
+                        <textarea name = "description" placeholder = "description"></textarea>
+                    </p>
+                    <p>
+                        ${template.authorSelect(authors)}
+                    <p>
+                    <p><input type = "submit"></p>
+                </form>`, '');
+            
+            response.writeHead(200);
+            response.end(html);
+        });
+    });
+}
+
+exports.create_process = function(request, response)
+{
+    var body = '';
+    
+    request.on('data', function(data)
+    {
+        body += data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query(`
+            INSERT INTO topic (title, description, created, author_id)
+                VALUES (?, ?, NOW(), ?)`,
+            [post.title, post.description, post.author],
+            function(error, result)
+            {
+                if(error) { throw error; }
+                
+                response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                response.end();
+            });
+    });
+}
+
+exports.update = function(request, response)
+{
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+
+    db.query(`SELECT * FROM topic`, function(error, topics)
+    {
+        if(error) { throw error; }
+
+        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic)
+        {
+            if(error2) { throw error2; }
+
+            db.query(`SELECT * FROM author`, function(error3, authors)
+            {
+                if(error3) { throw error3; }
+
+                var list = template.list(topics);
+                var html = template.HTML(topic[0].title, list,
+                    `<form action = "/update_process" method = "POST">
+                        <input type = "hidden" name = "id" value = "${topic[0].id}"
+                        <p>
+                            <input type = "text" name = "title" placeholder = "title" value = "${topic[0].title}">
+                        </p>
+                        <p>
+                            <textarea name = "description" placeholder = "description">${topic[0].description}</textarea>
+                        </p>
+                        <p>
+                            ${template.authorSelect(authors, topic[0].author_id)}
+                        <p><input type = "submit"></p>
+                    </form>`,
+                    `<a href = "/create>create</a> <a href = "/update?id=${topic[0].id}">update</a>`);
+            
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+    });
+}
+
+exports.update_process = function(request, response)
+{
+    var body = '';
+
+    request.on('data', function(data)
+    {
+        body += data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query(`UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
+            [post.title, post.description, post.author, post.id],
+            function(error, result)
+            {
+                if(error) { throw error; }
+
+                response.writeHead(302, {Location: `/?id=${post.id}`});
+                response.end();
+            });
+    });
+}
+
+exports.delete_process = function(request, response)
+{
+    var body = '';
+    
+    request.on('data', function(data)
+    {
+        body = body + data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query('DELETE FROM topic WHERE id=?', [post.id], function(error, result)
+        {
+            if(error) { throw error; }
+
+            response.writeHead(302, {Location: `/`});
+            response.end();
+        });
     });
 }
