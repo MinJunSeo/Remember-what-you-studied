6b95d93e5496a1bd81176dc30f71de996563f3f4
MinJun Seo <smj0385@naver.com>
2020-05-28 20:00:41 +0900

    docs : Update author.js
    새로 변경된 내용
    1. author 내용을 변경한 후 update 버튼을 누를 경우 해당 정보가 데이터베이스에도 반영되며 동시에 웹 페이지에도 반영됩니다.
    2. delete 버튼을 누르면 author의 정보와 author가 작성한 모든 글을 데이터베이스 및 웹 페이지 상에서 삭제합니다.
    3. sanitize-html module을 사용하여 사용자의 잘못된 입력으로부터 서버를 보호합니다.

--- Node.js/opentutorial_Nodejs/lib/author.js
+++ Node.js/opentutorial_Nodejs/lib/author.js
@@ -1,5 +1,8 @@
 var db = require('./db.js');
 var template = require('./template.js');
+var qs = require('querystring');
+var url = require('url');
+var sanitzieHtml = require('sanitize-html');
 
 exports.home = function(request, response)
 {
@@ -11,7 +14,7 @@ exports.home = function(request, response)
         {
             if(error2) { throw error2; }
 
-            var title = 'Welcome';
+            var title = 'author';
             var list = template.list(topics);
             var html = template.HTML(title, list,
                 `
@@ -26,11 +29,152 @@ exports.home = function(request, response)
                         border : 1px solid black;
                     }
                 </style>
-                `,
-                `<a href = "/create">create</a>`);
+                <form action = "/author/create_process" method = "POST">
+                    <p>
+                        <input type = "text" name = "name" placeholder = "name">
+                    </p>
+                    <p>
+                        <textarea name = "profile" placeholder = "profile"></textarea>
+                    </p>
+                    <p>
+                        <input type = "submit">
+                    </p>
+                </form>
+                `, ``);
 
             response.writeHead(200);
             response.end(html);
+        });
+    });
+}
+
+exports.create_process = function(request, response)
+{
+    var body = '';
+    
+    request.on('data', function(data)
+    {
+        body += data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query(`
+            INSERT INTO author (name, profile)
+                VALUES (?, ?)`,
+            [post.name, post.profile],
+            function(error, result)
+            {
+                if(error) { throw error; }
+                
+                response.writeHead(302, {Location: `/author`});
+                response.end();
+            });
+    });
+}
+
+exports.update = function(request, response)
+{
+    db.query(`SELECT * FROM topic`, function(error, topics)
+    {
+        if(error) { throw error; }
+
+        db.query(`SELECT * FROM author`, function(error2, authors)
+        {
+            if(error2) { throw error2; }
+            var _url = request.url;
+            var queryData = url.parse(_url, true).query;
+
+            db.query(`SELECT * FROM author WHERE id=?`, [queryData.id], function(error3, author)
+            {
+                if(error3) { throw error3; }
+
+                var title = 'author';
+                var list = template.list(topics);
+                var html = template.HTML(title, list,
+                    `
+                    ${template.authorTable(authors)}
+                    <style>
+                        table
+                        {
+                            border-collapse : collapse;
+                        }
+                        td
+                        {
+                            border : 1px solid black;
+                        }
+                    </style>
+                    <form action = "/author/update_process" method = "POST">
+                        <p>
+                            <input type = "hidden" name = "id" value = "${queryData.id}">
+                        </p>
+                        <p>
+                            <input type = "text" name = "name" value = "${sanitzieHtml(author[0].name)}" placeholder = "name">
+                        </p>
+                        <p>
+                            <textarea name = "profile" placeholder = "profile">${sanitzieHtml(author[0].profile)}</textarea>
+                        </p>
+                        <p>
+                            <input type = "submit" value = "update">
+                        </p>
+                    </form>
+                    `, ``);
+
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+    });
+}
+
+exports.update_process = function(request, response)
+{
+    var body = '';
+
+    request.on('data', function(data)
+    {
+        body += data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query(`UPDATE author SET name=?, profile=? WHERE id=?`,
+            [post.name, post.profile, post.id],
+            function(error, result)
+            {
+                if(error) { throw error; }
+
+                response.writeHead(302, {Location: '/author'});
+                response.end();
+            });
+    });
+}
+
+exports.delete_process = function(request, response)
+{
+    var body = '';
+    
+    request.on('data', function(data)
+    {
+        body = body + data;
+    });
+    request.on('end', function()
+    {
+        var post = qs.parse(body);
+
+        db.query(`DELETE FROM topic WHERE author_id=?`, [post.id], function(error1, result1)
+        {
+            if(error1) { throw error1; }
+            
+            db.query(`DELETE FROM author WHERE id=?`, [post.id], function(error2, result2)
+            {
+                if(error2) { throw error; }
+
+                response.writeHead(302, {Location: '/author'});
+                response.end();
+            });
         });
     });
 }
