2c98d07980fd8c55516cde8787e3700296d6d5d6
MinJun Seo <smj0385@naver.com>
2020-05-28 19:56:52 +0900

    docs : Update template.js
    새로 변경된 내용
    1. sanitize-html module을 사용하여 사용자의 잘못된 입력으로부터 서버를 보호합니다.

--- Node.js/opentutorial_Nodejs/lib/template.js
+++ Node.js/opentutorial_Nodejs/lib/template.js
@@ -1,3 +1,5 @@
+var sanitizeHtml = require('sanitize-html');
+
 module.exports = {
     HTML : function(title, list, body, control)
     {
@@ -24,7 +26,7 @@ module.exports = {
         var i = 0;
         while(i < topics.length)
         {
-            list = list + `<li><a href="/?id=${topics[i].id}">${topics[i].title}</a></li>`;
+            list = list + `<li><a href="/?id=${topics[i].id}">${sanitizeHtml(topics[i].title)}</a></li>`;
             i = i + 1;
         }
         list = list+'</ul>';
@@ -43,7 +45,7 @@ module.exports = {
             {
                 selected = ' selected';
             }
-            tag += `<option value = "${authors[i].id}"${selected}>${authors[i].name}</option>`;
+            tag += `<option value = "${authors[i].id}"${selected}>${sanitizeHtml(authors[i].name)}</option>`;
             i++;
         }
 
@@ -61,10 +63,15 @@ module.exports = {
         {
             tag += `
                 <tr>
-                    <td>${authors[i].name}</td>
-                    <td>${authors[i].profile}</td>
-                    <td>update</td>
-                    <td>delete</td>
+                    <td>${sanitzieHtml(authors[i].name)}</td>
+                    <td>${sanitzieHtml(authors[i].profile)}</td>
+                    <td><a href = "/author/update?id=${authors[i].id}">update</a></td>
+                    <td>
+                        <form action = "/author/delete_process" method = "POST">
+                            <input type = "hidden" name = "id" value = "${authors[i].id}">
+                            <input type = "submit" value = "delete">
+                        </form>
+                    </td>
                 </tr>
             `;
             i++;
